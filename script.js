3786e0a4c219d0873878275077e0e435afee3857
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-06-27 17:07:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1 = str.replace(" ","");
+	let end = str1.length-1;
+	let start =0;
+	while(start<end){
+		if(str1[start]!=str1[end]){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
