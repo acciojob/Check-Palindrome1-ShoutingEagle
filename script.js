a72913c0edb9351081a6746a3702bb337630903b
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-06-28 14:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	let str1 = str.replace(" ","");
+	let str1 = str.replaceAll(' ','');
 	let end = str1.length-1;
 	let start =0;
 	while(start<end){
