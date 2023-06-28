6d29b9b64d862518e1e07f76c36f62e939b899aa
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-06-28 14:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // complete the given function
 
 function palindrome(str){
-	let str1 = str.replaceAll(' ','');
+	let str1 = "";
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i)!=" "){
+			str1 = str1+str.charAt(i);
+		}
+	}
 	let end = str1.length-1;
 	let start =0;
 	while(start<end){
-		if(str1[start]!=str1[end]){
+		if(str1.charAt(start)!=str1.charAt(end)){
 			return false;
 		}
+		start++;
+		end--;
 	}
 	return true;
 }
